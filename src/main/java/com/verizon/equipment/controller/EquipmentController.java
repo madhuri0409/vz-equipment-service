4e9fca7ffb18c20204ebd28a2690b9aab52ab6da
Ganga Madhuri Maddala <gangamadhuri.m@ad.infosys.com>
2020-06-17 23:05:37 -0500

    Updated Equipment Service

--- src/main/java/com/verizon/equipment/controller/EquipmentController.java
+++ src/main/java/com/verizon/equipment/controller/EquipmentController.java
@@ -2,6 +2,8 @@ package com.verizon.equipment.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,11 +15,16 @@ import com.verizon.equipment.service.EquipmentService;
 @RestController
 public class EquipmentController {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(EquipmentController.class);
+	
 	@Autowired
 	private EquipmentService equipmentService;
 	
+	
+	
 	@GetMapping("/equipmentsById")
 	public List<EquipmentDetails> getEquipmemntsById(@RequestParam(value = "id") Integer id) {
+		LOGGER.info("Calling Equipment Service for customer id :  {}",id);
 		return equipmentService.getEquipmentDetailsByCustomerId(id);
 	}
 
--- src/main/java/com/verizon/equipment/service/EquipmentService.java
+++ src/main/java/com/verizon/equipment/service/EquipmentService.java
@@ -2,6 +2,8 @@ package com.verizon.equipment.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,13 @@ import com.verizon.equipment.repo.EquipmentDetailsRepo;
 @Service
 public class EquipmentService {
 
+	Logger LOGGER = LoggerFactory.getLogger(EquipmentService.class);
+	
 	@Autowired
 	private EquipmentDetailsRepo equipmentDetailsRepo;
 	
 	public List<EquipmentDetails> getEquipmentDetailsByCustomerId(Integer customerId){
+		LOGGER.info("In Equipment Service : Fetching Equipment Details  for Customer Id {}", customerId);
 		return equipmentDetailsRepo.findAllByCustomerId(customerId);
 	}
 	
