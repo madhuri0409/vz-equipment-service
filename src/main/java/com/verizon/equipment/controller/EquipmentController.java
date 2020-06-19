d53df7eeb1caa8784791041f585a5a3199fe98e0
Ganga Madhuri Maddala <gangamadhuri.m@ad.infosys.com>
2020-06-19 01:05:19 -0500

    updated Kafka in equipment

--- src/main/java/com/verizon/equipment/controller/EquipmentController.java
+++ src/main/java/com/verizon/equipment/controller/EquipmentController.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.verizon.equipment.entity.EquipmentDetails;
 import com.verizon.equipment.service.EquipmentService;
+
+import brave.Tracer;
 
 @RestController
 public class EquipmentController {
@@ -20,12 +24,25 @@ public class EquipmentController {
 	@Autowired
 	private EquipmentService equipmentService;
 	
+	@Autowired
+	private RestTemplate restTemplate;
 	
+	@Value("${kafka.service.uri}")
+	private String kafkaServiceUri;
+	
+	@Autowired
+	Tracer tracer;
 	
 	@GetMapping("/equipmentsById")
 	public List<EquipmentDetails> getEquipmemntsById(@RequestParam(value = "id") Integer id) {
+		Long currentTime = System.currentTimeMillis();
 		LOGGER.info("Calling Equipment Service for customer id :  {}",id);
-		return equipmentService.getEquipmentDetailsByCustomerId(id);
+		List<EquipmentDetails> equipmentdetails = equipmentService.getEquipmentDetailsByCustomerId(id);
+		
+		String message = String.format("%d | %s | %s | %s | %s | %s", currentTime, "EquipmentService", tracer.currentSpan().context().spanIdString(), tracer.currentSpan().context().traceIdString(), id, equipmentdetails);
+		restTemplate.getForObject(kafkaServiceUri + "=" + message, String.class);
+		
+		return equipmentdetails;
 	}
 
 }
